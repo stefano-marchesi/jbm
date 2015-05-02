74aa276dff39e2d925ed33dd0e0801daae40cc94
Stefano Marchesi <stefano.marchesi1984@gmail.com>
2015-05-02 18:25:16 +0200

    oops

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+var serveStatic = require('serve-static')
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var multer = require('multer');
